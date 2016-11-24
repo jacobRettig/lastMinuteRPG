5abe426cc004cc44ecbaf91f22cf7afe6725666e
jacobRettig <jacob13rettig@gmail.com>
2016-11-24 17:11:38 -0600

    Revert "Added program's graphics and system framework:"
    This reverts commit 0a3a172353f2a46cd8e671021fdbdd8ec4a0fa16.

--- game.c
+++
@@ -1,9 +0,0 @@
-#include "tex.h"
-#include "vid.h"
-#include "game.h"
-
-void game(vid_t *vid,tex_t *tex_root,float tstep){
-
-	vid_clear_shade(vid,128);
-
-}
--- game.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "vid.h"
-#include "tex.h"
-
-void game(vid_t *vid,tex_t *tex_root,float tstep);
